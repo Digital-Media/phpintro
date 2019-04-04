4ac52054581e666f30aea6c0264803e04aaf3945
Harrer Martin <P20137@fhooe.at>
2019-04-04 12:29:41 +0200

    Update Login.php

--- src/Exercises/Login.php
+++ src/Exercises/Login.php
@@ -124,13 +124,5 @@ final class Login extends AbstractNormForm
         //#%#%
 
         //%%login/authenticateUser
-        $users = $this->fileAccess->loadContents(self::USER_DATA_PATH);
-
-        foreach ($users as $user) {
-            if ($user[self::USERNAME] === $_POST[self::USERNAME] && password_verify($_POST[self::PASSWORD], $user[self::PASSWORD])) {
-                return true;
-            }
-        }
-        return false;
     }
 }
